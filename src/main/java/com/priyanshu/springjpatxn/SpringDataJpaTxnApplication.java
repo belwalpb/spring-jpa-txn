cdb29c2843ae3d316e60e568f48029ba92669c5b
Priyanshu Belwal <priyanshubelwalpb@gmail.com>
2023-06-17 09:48:53 +0530

    Initial Commit

---
+++ src/main/java/com/priyanshu/springjpatxn/SpringDataJpaTxnApplication.java
@@ -0,0 +1,13 @@
+package com.priyanshu.springjpatxn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaTxnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaTxnApplication.class, args);
+	}
+
+}
